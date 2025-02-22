1492572b4ba48ef17b3b57a4bf0201757adbb6be
saurabh singh <singhsaurabh.hkps@gmail.com>
2025-02-23 04:58:35 +0530

    initial comit

---
+++ new.js
@@ -0,0 +1,7 @@
+let firstName = "John";     // Variable that can be reassigned
+const age = 30;        // Constant variable that cannot be reassigned
+var isStudent = true;  // Older way to declare variables, function-scoped
+
+console.log(firstName);
+console.log(age);
+console.log(isStudent);
